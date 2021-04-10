0ee95dde017fdaece64893b469a4cda54c341fe5
medacaMan <ymd2007umd@gmail.com>
2021-04-10 17:16:38 +0900

    002 追加

---
+++ 002/Source.cpp
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable: 4244) // 最悪をします
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <map> 
+#include <set>
+#include <string>
+#include <functional>
+#include <list>
+#include <random>
+#include <time.h>
+#include <iomanip>
+#include <assert.h>
+#include <numeric>
+#include <sstream>
+#define BIT(nr) (1UL << (nr))
+//#define int long long
+//#define ll long long
+#define double long double
+#define mod 1000000007
+#define MAXN (int)1e+5 * 2+1
+#define LL_MAX 9223372036854775807	//ない環境用
+#define LL_HALFMAX 9223372036854775807 / 2	//ない環境用
+#define MIN -(9223372036854775807 / 2)
+#define REP(i,a,n) for(int i=(a); i<(int)(n); i++)
+#define rep(i,n) REP(i,0,n)
+#define FOR(it,c) for(__typeof((c).begin()) it=(c).begin(); it!=(c).end(); ++it)
+#define ALLOF(c) (c).begin(), (c).end()
+#define REPS(i,x) for(int i=1;i<=(int)(x);i++)
+#define RREP(i,x) for(int i=((int)(x)-1);i>=0;i--)
+#define RREPS(i,x) for(int i=((int)(x));i>0;i--)
+#define repl(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
+#define mp make_pair
+template<typename T1, typename T2> inline void chmin(T1 & a, T2 b) { if (a > b) a = b; }
+template<typename T1, typename T2> inline void chmax(T1& a, T2 b) { if (a < b) a = b; }
+
+
+using namespace std;
+
+//デバッグ用カッコの有無
+#ifdef DEBUG
+template <class T>ostream &operator<<(ostream &o, const vector<T>&v)
+{
+	o << "{"; for (int i = 0; i<(int)v.size(); i++)o << (i>0 ? ", " : "") << v[i]; o << "}"; return o;
+}
+#endif // DEBUG
+
+template <class T>ostream &operator<<(ostream &o, const vector<T>&v)
+{
+	for (int i = 0; i<(int)v.size(); i++)o << (i>0 ? " " : "") << v[i]; return o;
+}
+
+int dx[4] = { 0, 1, 0, -1 }; // x軸方向への変位
+int dy[4] = { 1, 0, -1, 0 }; // y軸方向への変位
+
+int dxp[4] = { 0, 1 }; // x軸方向への変位(正のみ)
+int dyp[4] = { 1, 0 }; // y軸方向への変位(負のみ)
+
+signed main() {
+	int N;
+	cin >> N;
+
+	vector<string> anses;
+
+	rep(S, 1LL << N) {
+		// 1: ) 0: (
+		int now = 0;
+		bool ok = true;
+		string ans;
+		rep(i, N) {
+			now += (S & (1LL << i) ? -1 : 1);
+			ans += (S & (1LL << i) ? ")" : "(");
+			if (now < 0) {
+				ok = false;
+			}
+		}
+
+		if (ok && now == 0) {
+			anses.push_back(ans);
+		}
+	}
+
+	sort(ALLOF(anses));
+
+	for (auto s : anses) {
+		cout << s << "\n";
+	}
+
+}
